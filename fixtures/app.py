f1dde00ab9db48e66fda3dbdd6a0809ae75e9543
Andrew Tabalin <tjandrewex@gmail.com>
2023-08-12 21:53:39 +0300

    Add register new user test wuth valid data

--- fixtures/app.py
+++ fixtures/app.py
@@ -1,4 +1,9 @@
+from fixtures.register.api import RegisterController
+from fixtures.requests import Client
+
 
 class ClientApp:
     def __init__(self, url):
         self.url = url
+        self.client = Client
+        self.register = RegisterController(self)
